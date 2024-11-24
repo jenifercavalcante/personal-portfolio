d3b71b631133a178470447d978f91bd82cf5eaf3
Jenifer Cavalcante <jennifercavalcante.28@gmail.com>
2024-11-24 18:24:45 -0300

    feat: adjust mobile and desktop templates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import PersonalDescription from '@/components/Home/PersonalDescription';
 import Terminal from '@/components/Terminal/Terminal';
 
+import '@/styles/home/index.scss';
+
 export default function Home() {
   return (
     <div className="home-container">
